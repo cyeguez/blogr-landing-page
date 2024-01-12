697921687a7e1819c1ffae6a07b15178459e3c84
Cesar Yeguez <Cesar.Yeguez@gmail.com>
2024-01-12 00:19:17 -0300

    Culminación del header mobile al 100%

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const btnMenu = document.querySelector(".icon-menu");
 const nav = document.querySelector(".nav");
 const btnClose = document.querySelector(".icon-close");
-const subMenu= document.querySelector(".sub-menu");
+const subMenu = document.querySelector(".sub-menu");
+const conetMenu = document.querySelector(".conect-submenu");
 
 btnMenu.addEventListener("click", () => {
   nav.classList.remove("hidden");
@@ -15,7 +16,14 @@ btnClose.addEventListener("click", () => {
   btnClose.style.display = "none";
 });
 
-
 subMenu.addEventListener("click", () => {
-  
+  conetMenu.classList.toggle("hidden-conet");
+  subMenu.classList.toggle("active");
+});
+
+conetMenu.addEventListener("mouseleave", () => {
+  conetMenu.classList.add("hidden-conet");
+  subMenu.classList.remove("active");
+
+
 });
